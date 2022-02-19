fca21e1fbff5251934932c8187108575b47c0ceb
Julian Yap <julianokyap@gmail.com>
2022-02-18 19:49:51 -0800

    web3_clientVersion: Sparrow/v

--- app/scripts/controllers/network/createMetamaskMiddleware.js
+++ app/scripts/controllers/network/createMetamaskMiddleware.js
@@ -22,7 +22,7 @@ export default function createMetamaskMiddleware({
   const metamaskMiddleware = mergeMiddleware([
     createScaffoldMiddleware({
       eth_syncing: false,
-      web3_clientVersion: `MetaMask/v${version}`,
+      web3_clientVersion: `Sparrow/v${version}`,
     }),
     createWalletMiddleware({
       getAccounts,
